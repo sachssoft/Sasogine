e3f6e50b81d18aef2d5a76d5a0338c4c20b3d80c
sachssoft <60543974+sachssoft@users.noreply.github.com>
2026-06-04 09:58:53 +0200

    Remove

--- Basic/Components/ComponentDefinitionRegistry.cs
+++
@@ -1,8 +0,0 @@
-﻿using Sachssoft.Sasogine.Common;
-
-namespace Sachssoft.Sasogine.Components
-{
-    public sealed class ComponentDefinitionRegistry : DefinitionRegistry<IComponent, IComponentDefinition>
-    {
-    }
-}
