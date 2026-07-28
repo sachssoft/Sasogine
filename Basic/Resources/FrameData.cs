c50816369d385a28d21132debd1e26fb245d3c25
sachssoft <60543974+sachssoft@users.noreply.github.com>
2026-07-28 16:16:09 +0200

    Introduce new frame set system

---
+++ Basic/Resources/FrameData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Sachssoft.Sasogine.Common;
+
+namespace Sachssoft.Sasogine.Resources;
+
+/// <summary>
+/// Represents a single frame region inside a texture atlas.
+/// 
+/// A frame defines the pixel location, size, and source rectangle
+/// used to extract a region from the texture during rendering.
+/// 
+/// The structure is immutable and optimized for frequent access
+/// during rendering and batching operations.
+/// </summary>
+public readonly struct FrameData : ISourceRegion
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FrameData"/> struct.
+    /// </summary>
+    /// <param name="location">
+    /// Pixel location of the frame inside the texture atlas.
+    /// </param>
+    /// <param name="size">
+    /// Size of the frame in pixels.
+    /// </param>
+    public FrameData(
+        Point location,
+        PixelSize size)
+    {
+        Location = location;
+        Size = size;
+
+        SourceRectangle = new Rectangle(
+            location.X,
+            location.Y,
+            size.Width,
+            size.Height);
+    }
+
+
+    /// <summary>
+    /// Gets the pixel location of the frame inside the texture atlas.
+    /// </summary>
+    public Point Location { get; }
+
+
+    /// <summary>
+    /// Gets the size of the frame in pixels.
+    /// </summary>
+    public PixelSize Size { get; }
+
+
+    /// <summary>
+    /// Gets the rectangular source region used for rendering.
+    /// </summary>
+    public Rectangle SourceRectangle { get; }
+}
---
+++ Basic/Resources/KeyedFrameSet.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Sachssoft.Sasogine.Assets.Graphics;
+using Sachssoft.Sasogine.Common;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Sachssoft.Sasogine.Resources;
+
+/// <summary>
+/// Represents a tile frame set where frames are accessed using string keys.
+///
+/// This implementation is mainly intended for asset loading, editor usage,
+/// and content definitions where readable frame names are useful.
+/// </summary>
+public sealed class KeyedFrameSet : IFrameSet
+{
+    private readonly Dictionary<string, FrameData> _frames = new();
+
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="KeyedFrameSet"/> class.
+    /// </summary>
+    /// <param name="asset">
+    /// Texture asset containing the frame data.
+    /// </param>
+    public KeyedFrameSet(
+        Texture2DAsset asset)
+    {
+        Asset = asset;
+    }
+
+
+    /// <summary>
+    /// Gets the texture asset containing the frames.
+    /// </summary>
+    public Texture2DAsset Asset { get; }
+
+    /// <summary>
+    /// Gets all string keys used to identify the registered tile frames.
+    /// </summary>
+    public IEnumerable<string> Keys
+        => _frames.Keys;
+
+    IEnumerable<object> IFrameSet.Keys
+        => _frames.Keys;
+
+
+    /// <summary>
+    /// Gets the frame associated with the specified key.
+    /// </summary>
+    /// <param name="key">
+    /// String key of the frame.
+    /// </param>
+    public FrameData this[string key]
+        => _frames[key];
+
+    FrameData IFrameSet.this[object key]
+        => this[(string)key];
+
+
+    /// <summary>
+    /// Adds a frame using a string key and atlas position.
+    /// </summary>
+    /// <param name="key">
+    /// Key used to identify the frame.
+    /// </param>
+    /// <param name="position">
+    /// Position of the frame inside the texture atlas.
+    /// </param>
+    /// <param name="size">
+    /// Pixel size of the frame.
+    /// </param>
+    public void Add(
+        string key,
+        Point position,
+        PixelSize size)
+    {
+        _frames.Add(
+            key,
+            new FrameData(position, size));
+    }
+
+
+    /// <summary>
+    /// Returns an enumerator that iterates through all tile frames.
+    /// </summary>
+    public IEnumerator<FrameData> GetEnumerator()
+    {
+        return _frames.Values.GetEnumerator();
+    }
+
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
