5f754573bb1b2ce68b1e8b108937469f6a03f87d
sachssoft <60543974+sachssoft@users.noreply.github.com>
2026-09-17 23:12:39 +0200

    Move

--- Projects/Experimental/Basic/Graphics/Meshes/MeshVertexFactory.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Xna.Framework.Graphics;
-
-namespace Sachssoft.Sasogine.Experimental.Graphics.Meshes;
-
-/// <summary>
-/// Creates a concrete GPU vertex from generated mesh vertex data.
-/// </summary>
-public delegate TVertex MeshVertexFactory<TVertex>(in MeshVertexData data)
-    where TVertex : struct, IVertexType;
